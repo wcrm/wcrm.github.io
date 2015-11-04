b7c25653639734b8a8f354272062e1608e46c329
wcrm <wcrmcgowan@gmail.com>
2015-11-04 02:20:19 -0500

    d
    d

--- config.php
+++ config.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('./stripe-php/init.php');
+
 $stripe = array(
   "secret_key"      => "sk_test_nzLEJuOBlN8uiPYG3bWrswoJ",
   "publishable_key" => "pk_test_PINRzPvBXGYVB2Pe2WBl8yjB"
